08c838537ac19fb602b79bfbc945ceaf5fb804b7
Geovani José <geovani-lol@hotmail.com>
2020-02-01 02:32:55 -0300

    Implement Footer component

--- desktop/src/App.js
+++ desktop/src/App.js
@@ -2,10 +2,14 @@ import React from 'react'
 import './App.css'
 
 import Main from './pages/Main'
+import Footer from './components/Footer'
 
 function App () {
   return (
-    <Main />
+    <>
+      <Main />
+      <Footer />
+    </>
   )
 }
 
