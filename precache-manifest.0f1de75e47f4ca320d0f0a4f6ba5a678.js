649794801a2fdf906c7abbe6943a3ab615c949d0
Geovani José <geovani-lol@hotmail.com>
2020-02-01 03:55:30 -0300

    Updates

---
+++ precache-manifest.0f1de75e47f4ca320d0f0a4f6ba5a678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce7f5381900061c7493641fe784d052",
+    "url": "/provawoozav/index.html"
+  },
+  {
+    "revision": "e1b2176feaa9e232b58a",
+    "url": "/provawoozav/static/css/main.365b9538.chunk.css"
+  },
+  {
+    "revision": "5b21e5171c3a432563ed",
+    "url": "/provawoozav/static/js/2.5d3324b1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/provawoozav/static/js/2.5d3324b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1b2176feaa9e232b58a",
+    "url": "/provawoozav/static/js/main.ffdf462c.chunk.js"
+  },
+  {
+    "revision": "eb8d3143eb3eb6e6c544",
+    "url": "/provawoozav/static/js/runtime-main.258548e2.js"
+  }
+]);
